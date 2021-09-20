61f232d641b05ecc1e7db3a65cb9c54922a503a3
samaritan7 <urselpaap@gmail.com>
2021-09-20 16:55:44 +0300

    removed node_modules

---
+++ backend/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+        _id:'1',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price:40,
+        brand: 'Nike',
+        rating: 4.2,
+        numReviews: 7,
+    },
+    {
+        _id:'2',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price:70,
+        brand: 'Nike',
+        rating: 4.1,
+        numReviews: 10,
+    },
+    {
+        _id:'3',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price:60,
+        brand: 'Nike',
+        rating: 4.9,
+        numReviews: 8,
+    },
+    {
+        _id:'4',
+        name: 'Loose Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price:60,
+        brand: 'Reebok',
+        rating: 4.9,
+        numReviews: 8,
+    },
+    {
+        _id:'5',
+        name: 'Short Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price:30,
+        brand: 'Fila',
+        rating: 4.9,
+        numReviews: 8,
+    },
+    {
+        _id:'6',
+        name: 'V-neck shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price:25,
+        brand: 'Adidas',
+        rating: 4.9,
+        numReviews: 8,
+    },
+]
+};
+
+export default data;
