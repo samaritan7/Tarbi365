12c1a5afe45b064e48d9783fc79c20d49dc35e3c
samaritan7 <urselpaap@gmail.com>
2021-09-22 14:08:37 +0300

    fetch server data with React Hooks

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
 
 function HomeScreen (props) {
+
+  const[products, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      const {data} = await axios.get("/api/products");
+      setProduct(data);
+
+    }
+    fetchData();
+    return () => {
+      
+    };
+  }, [])
+
     return  <ul className="products">
     {
-      data.products.map(product =>
-      <li>
+      products.map(product =>
+      <li key ={product.id}>
         <div className="product">
         <Link to={'/product/' + product._id}>
         <img className="product-image" src={product.image} alt="product" />
@@ -23,4 +38,4 @@ function HomeScreen (props) {
     
   </ul>
 }
-export default HomeScreen;
+export default HomeScreen ;
